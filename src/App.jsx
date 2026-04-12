b78e3d4eb5d3081faea9c958cbec4e5efe09e29b
vadimk321 <8246vadim8246@mail.ru>
2026-04-12 17:53:46 +0300

    Add useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useReducer, useState, useEffect } from 'react';
 import './app.css'
 
 import TaskList from './TaskList.jsx'
 
+function tasksReducer(state, action){
+    switch (action.type) {
+      case 'ADD_TASK':
+        return [...state, action.payload];
+
+      case 'DELETE_TASK':
+        return state.filter(task => task.id !== action.payload)
+
+      case 'TOGGLE_TASK':
+        return state.map(task => 
+          task.id === action.payload 
+            ? {...task, done: !task.done}
+            : task
+        )
+      case 'EDIT_TASK':
+        return state.map(task => task.id === action.payload ? {...task, text:action.text } : task);
+
+      case 'CLEAR_TASKS':
+        return [];
+
+      case 'TOGGLE_ALL':
+        const hasActive = state.some(task => !task.done);
+
+        return (state.map(task => ({...task, done: hasActive ? true : false})));
+        
+      default:
+        return state
+    }
+}
+
 function App() {
 
-  const [tasks, setTasks] = useState(() => {
+  
+  const [tasks, dispatch] = useReducer(tasksReducer, [], () => {
     const saved = localStorage.getItem('tasks');
     return saved ? JSON.parse(saved) : [];
   });
-  const [editingId, setEditingId] = useState('');
 
+  const [editingId, setEditingId] = useState('');
 
 
   useEffect(() => {
@@ -22,53 +53,44 @@ function App() {
     e.preventDefault();
 
     const taskText = e.target.elements.text.value;
-    if (!taskText){
-      return
-    }
+    if (!taskText) return;
 
-    const newTask = {
-      id: new Date().toISOString(),
-      text: taskText,
-      done: false
-    }
-
-    setTasks(prev => [...prev, newTask]);
+    dispatch({
+      type: 'ADD_TASK',
+      payload: {
+        id: new Date().toISOString(),
+        text: taskText,
+        done: false
+      }
+    })
 
     e.target.reset();
   }
 
-  const deleteTask = useCallback((id) => {
-    setTasks(prev => prev.filter(task => task.id !== id))
-  }, [])
+  const deleteTask = (id) => {
+    dispatch({type: 'DELETE_TASK', payload: id});
+  };
 
-  const toggleTask = useCallback((id) => {
-    setTasks(prev => 
-      prev.map(task => 
-        task.id === id 
-        ? { ...task, done: !task.done} 
-        : task))
-  }, [])
+  const toggleTask = (id) => {
+    dispatch({type: 'TOGGLE_TASK', payload: id});
+  }
 
-  const startEdit = useCallback(id => {
-    setEditingId(id);
-  }, [])
-
-  const saveEdit = useCallback((id, newText) => {
-    setTasks(prev => {
-      return prev.map(task => task.id === id ? {...task, text: newText} : task)
-    })
+  const saveEdit = (id, newText) => {
+    dispatch({type: 'EDIT_TASK', payload: id, text: newText})
 
     setEditingId('');
-  }, [])
+    }
+    
+  const clearList = () => {
+    dispatch({type: 'CLEAR_TASKS'});
+  };
 
-  const cancelEdit = useCallback(() => {
-    setEditingId('');
-  }, [])
+  const toggleAllTasks = () => {
+    dispatch({type: 'TOGGLE_ALL'})
+  }
 
-  const clearList = useCallback(() => {
-    setTasks([]);
-  }, [])
-
+  const startEdit = (id) => setEditingId(id);
+  const cancelEdit = () => setEditingId('');
 
   return (
     <div>
@@ -87,6 +109,7 @@ function App() {
           />
         <hr />
         <button onClick={clearList}>Сбросить все задачи</button>
+        <button onClick={toggleAllTasks}>Переключить все задачи</button>
         <hr />
     </div>
   );
